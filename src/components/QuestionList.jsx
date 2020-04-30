1978c98e79d9fce93cf4c20775ae9e11f302d7ac
edwinisnow <edwin04@gmail.com>
2020-04-30 16:59:57 +0100

    Container Element mapStateToProps

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -9,13 +9,13 @@ import {
  * Each entry in the QuestionList is represtented by a QuestionListItem, which displays high-level information
  * about a question in a format that works well in a list
  */
-const QuestionListItem = ({tags,answer_count,title,views,question_id})=>(
+const QuestionListItem = ({ tags, answer_count, title, views, question_id }) => (
     <div className="mb-3">
         <h3>
             {title}
         </h3>
         <div className="mb-2">
-            <TagsList tags={tags}/>
+            <TagsList tags={tags} />
         </div>
         <div>
             <Link to={`/questions/${question_id}`}>
@@ -27,11 +27,11 @@ const QuestionListItem = ({tags,answer_count,title,views,question_id})=>(
 /**
  * Display all questions in an array provided to it as a simple list
  */
-const QuestionList = ({questions})=>(
+const QuestionList = ({ questions }) => (
     <div>
-        { questions ?
+        {questions ?
             <div>
-                {questions.map(question=><QuestionListItem key={question.question_id} {...question}/>)}
+                {questions.map(question => <QuestionListItem key={question.question_id} {...question} />)}
             </div> :
             <div>
                 Loading questions...
@@ -44,7 +44,7 @@ const QuestionList = ({questions})=>(
  * Get the list of questions from the application's state
  * It is populated by a ../sagas/fetch-question(s)-saga.
  */
-const mapStateToProps = ({questions})=>({
+const mapStateToProps = ({ questions }) => ({
     questions
 });
 
